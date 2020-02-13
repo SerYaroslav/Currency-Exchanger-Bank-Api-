940c67830699202ce7c6f4b991e73c63129351e0
Yaroslav Serediuk <seredyukua@gmail.com>
2020-02-13 20:33:54 +0200

    created bank service

---
+++ src/bank-service.js
@@ -0,0 +1,65 @@
+export default class BankService {
+
+  _apiBase = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?';
+
+  now = new Date();
+  today = 'today';
+  weekAgo = '7 days ago';
+  monthAgo = '30 days ago';
+  yearAgo = '1 year ago';
+
+  setDate = (date,fromWhen) => {
+  let res = new Date()
+  switch (fromWhen) {
+    case 'today':
+      return res = date;
+    
+    case '7 days ago':
+      return (res.setDate(date.getDate()-7));
+
+    case '30 days ago':
+      return (res.setDate(date.getDate()-30));
+
+    case '1 year ago':
+      return (res.setYear(date.getFullYear()-1));
+  
+    default:
+      return date
+  } 
+  }
+  
+  getDateCode = (date,fromWhen) => {
+  const neddedDate = this.setDate(date, fromWhen)
+
+  const neddedYear = new Date(neddedDate).getFullYear();
+
+  let neddedMonth = new Date(neddedDate).getMonth();
+    neddedMonth++
+    if(neddedMonth < 10) neddedMonth = "0" + neddedMonth;
+
+  let neddedDayDate = new Date(neddedDate).getDate();
+    if (neddedDayDate < 10) neddedDayDate = "0" + neddedDayDate;
+
+  const finalDate = `${neddedYear}${neddedMonth}${neddedDayDate}`; 
+
+  return finalDate;
+  };
+
+  getResourse = async (url) => {
+    const res = await fetch(`${this._apiBase}${url}&json`);
+
+    if(!res.ok) {
+      throw new Error(`Could not fetch ${url}, receive ${res.status}` )
+    }
+
+    return await res.json();
+  };
+
+  getRate = async(valcode, fromWhen, date = this.now) => {
+    const dateCode = this.getDateCode(date, fromWhen);
+    console.log(dateCode);
+    const res = await this.getResourse(`valcode=${valcode}&date=${dateCode}`);
+    return res;
+  };
+
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import store from "./store";
+
 import App from './components/app';
+import ErrorBoundry from "./components/error-boundry";
+import { ServiceProvider } from './components/service-context';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundry>
+      <ServiceProvider>
+        <App />
+      </ServiceProvider>
+    </ErrorBoundry>
+  </Provider>,
+  document.getElementById("root")
+);
