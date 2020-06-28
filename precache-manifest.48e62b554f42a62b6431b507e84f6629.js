e2687c76c356821d8f2699c49529261c27e74596
Yaroslav Serediuk <seredyukua@gmail.com>
2020-06-28 22:10:36 +0300

    Updates

---
+++ precache-manifest.48e62b554f42a62b6431b507e84f6629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29e583cf028cdb9c20341bac8414d9c",
+    "url": "/CurrencyExchanger/index.html"
+  },
+  {
+    "revision": "ebc50303132361f95189",
+    "url": "/CurrencyExchanger/static/css/main.8020e78a.chunk.css"
+  },
+  {
+    "revision": "a445435dd30cbd2c6ff8",
+    "url": "/CurrencyExchanger/static/js/2.00ad4972.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/CurrencyExchanger/static/js/2.00ad4972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc50303132361f95189",
+    "url": "/CurrencyExchanger/static/js/main.fd9a3e0f.chunk.js"
+  },
+  {
+    "revision": "b644feee47ea165747cb",
+    "url": "/CurrencyExchanger/static/js/runtime-main.4b2fdf65.js"
+  }
+]);
